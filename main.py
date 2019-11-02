84a106ca2cc552286c582b45edc1db92ad857fe1
JayanthVishalReddy <49403546+JayanthVishalReddy@users.noreply.github.com>
2019-11-02 11:21:13 +0530

    adding antecedents functoin

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import numpy as np
 import skfuzzy as fuzzy
 import matplotlib.pyplot as plt
 from skfuzzy import control as ctrl
+
+Physics = ctrl.Antecedent(np.arange(0,101,1),'Physics')
+Maths = ctrl.Antecedent(np.arange(0,101,1),'Maths')
+Chemistry = ctrl.Antecedent(np.arange(0,101,1),'Chemistry')
+Biology = ctrl.Antecedent(np.arange(0,101,1),'Biology')
+
