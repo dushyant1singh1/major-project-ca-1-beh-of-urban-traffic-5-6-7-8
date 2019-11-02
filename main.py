c626b3788e43d2bf3e9e169017255ffc6f2d35d6
JayanthVishalReddy <49403546+JayanthVishalReddy@users.noreply.github.com>
2019-11-02 11:31:51 +0530

    given range of marks

--- main.py
+++ main.py
@@ -12,3 +12,27 @@ Chemistry = ctrl.Antecedent(np.arange(0,101,1),'Chemistry')
 Biology = ctrl.Antecedent(np.arange(0,101,1),'Biology')
 
 
+#using trapmf(Trapizoidal membership Function)
+Physics['Excellent'] = fuzzy.trapmf(Physics.universe,[70,85,100,100])
+Physics['Good'] = fuzzy.trapmf(Physics.universe,[55,70,75,75])
+Physics['Average'] = fuzzy.trapmf(Physics.universe,[38,55,60,60])
+Physics['Poor'] = fuzzy.trapmf(Physics.universe,[25,30,40,40])
+Physics['Very_Poor'] = fuzzy.trapmf(Physics.universe,[0,15,25,25])
+
+Maths['Excellent'] = fuzzy.trapmf(Maths.universe,[70,85,100,100])
+Maths['Good'] = fuzzy.trapmf(Maths.universe,[55,70,75,75])
+Maths['Average'] = fuzzy.trapmf(Maths.universe,[38,55,60,60])
+Maths['Poor'] = fuzzy.trapmf(Maths.universe,[25,30,40,40])
+Maths['Very_Poor'] = fuzzy.trapmf(Maths.universe,[0,15,25,25])
+
+Chemistry['Excellent'] = fuzzy.trapmf(Chemistry.universe,[70,85,100,100])
+Chemistry['Good'] = fuzzy.trapmf(Chemistry.universe,[55,70,75,75])
+Chemistry['Average'] = fuzzy.trapmf(Chemistry.universe,[38,55,60,60])
+Chemistry['Poor'] = fuzzy.trapmf(Chemistry.universe,[25,30,40,40])
+Chemistry['Very_Poor'] = fuzzy.trapmf(Chemistry.universe,[0,15,25,25])
+
+Biology['Excellent'] = fuzzy.trapmf(Biology.universe,[70,85,100,100])
+Biology['Good'] = fuzzy.trapmf(Biology.universe,[55,70,75,75])
+Biology['Average'] = fuzzy.trapmf(Biology.universe,[38,55,60,60])
+Biology['Poor'] = fuzzy.trapmf(Biology.universe,[25,30,40,40])
+Biology['Very_Poor'] = fuzzy.trapmf(Biology.universe,[0,15,25,25])
